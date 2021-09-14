06947f2fc8e15f4bddd5ea42a9225915f6df66e5
Avinash7564 <avinashkumarmbns@gmail.com>
2021-09-15 01:53:28 +0530

    modified projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -31,7 +31,7 @@ function Projects() {
               title="Agoda Clone "
               description="Agoda is an online travel agency and metasearch engine for hotels, vacation rentals, flights, and airport transfer. It was founded in 2003 and later acquired by Booking Holdings, becoming a subsidiary of the company."
               link="https://github.com/Avinash7564/Agoda-Clone"
-              linkmedium=""
+              linkmedium="https://avinashrexx.hashnode.dev/cloning-of-agoda-website"
               linklive="http://Agoda.netlify.app"
             />
           </Col>
@@ -54,44 +54,11 @@ function Projects() {
               isBlog={false}
               title="Bluemercurry Clone"
               description="A cloned web application of bluemercury.com, a US-based website that sells cosmetics products and provides a wide range of products to choose from"
-              link=""
-              linkmedium=""
-              linklive=""
+              link="https://github.com/Avinash7564/bluemercurry_clone"
+              linkmedium="https://avinashrexx.hashnode.dev/bluemercury-website-cloning"
+              linklive="bluemercurryclone.netlify.app"
             />
           </Col>
-
-          {/* <Col md={5} className="project-card" data-aos="fade-right">
-            <ProjectCard
-              imgPath={bitsOfCode}
-              isBlog={false}
-              title="Animoto Clone"
-              description="Animoto is a cloud-based video creation service that produces video from photos, video clips, and music into video slideshows,and customized web-based presentations."
-              link="https://github.com/nilpatil7744/Project_Damodar"
-              linkmedium="https://nilp7744.medium.com/masai-collaboration-project-animoto-clone-fec2a84b2f8b"
-              linklive="https://animoto-clone.netlify.app/"
-            />
-          </Col> */}
-
-          {/* <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={suicide}
-              isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              link="https://github.com/soumyajit4419/AI_For_Social_Good"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={emotion}
-              isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              link="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-            />
-          </Col> */}
         </Row>
       </Container>
     </Container>
